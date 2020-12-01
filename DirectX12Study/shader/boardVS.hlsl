eb9da2536892e3eb890c06627e9abcdf591dc1cd
Trinh Le Hai Nam <trinhlehainamjapan@gmail.com>
2020-12-01 22:34:13 +0900

    Continue Refactoring

--- DirectX12Study/shader/boardVS.hlsl
+++ DirectX12Study/shader/boardVS.hlsl
@@ -4,6 +4,9 @@ BoardOutput boardVS(float4 pos : POSITION)
 {
 	BoardOutput ret;
 	ret.svpos = pos;
+
+	// transform NDC (Normalized Device Coordinate) to TEXCOORD (Texture Coordinate)
 	ret.uv = (pos.xy * float2(1, -1) + float2(1, 1)) * 0.5f;
+
 	return ret;
 }
