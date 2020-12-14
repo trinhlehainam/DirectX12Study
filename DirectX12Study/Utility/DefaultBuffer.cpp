e61283a2e89cf4f5b713c17e5654bddd6a9c439f
Trinh Le Hai Nam <trinhlehainamjapan@gmail.com>
2020-12-14 17:38:27 +0900

    Create Default Buffer class

--- DirectX12Study/Utility/DefaultBuffer.cpp
+++ DirectX12Study/Utility/DefaultBuffer.cpp
@@ -1,1 +1,80 @@
 #include "DefaultBuffer.h"
+#include "D12Helper.h"
+#include <cassert>
+
+DefaultBuffer::~DefaultBuffer()
+{
+    if (m_subresource != nullptr)
+        delete m_subresource;
+    m_subresource = nullptr;
+}
+
+ID3D12Resource* DefaultBuffer::Resource()
+{
+    return m_buffer.Get();
+}
+
+D3D12_GPU_VIRTUAL_ADDRESS DefaultBuffer::GetGPUVirtualAddress()
+{
+    return m_buffer.Get()->GetGPUVirtualAddress();
+}
+
+void DefaultBuffer::SetUpSubresource(const void* pData, LONG_PTR RowPitch, LONG_PTR SlicePitch,
+    bool IsShaderResourceBuffer)
+{
+    if (m_subresource != nullptr) delete m_subresource;
+    m_subresource = new D3D12_SUBRESOURCE_DATA();
+
+    m_subresource->pData = pData;
+    m_subresource->RowPitch = RowPitch;
+    m_subresource->SlicePitch = SlicePitch;
+
+    m_isShaderResourceBuffer = IsShaderResourceBuffer;
+}
+
+void DefaultBuffer::SetUpSubresource(const void* pData, size_t SizeInBytes)
+{
+    if (m_subresource != nullptr) delete m_subresource;
+    m_subresource = new D3D12_SUBRESOURCE_DATA();
+
+    m_subresource->pData = pData;
+    m_subresource->RowPitch = SizeInBytes;
+    m_subresource->SlicePitch = SizeInBytes;
+
+    m_isShaderResourceBuffer = false;
+}
+
+bool DefaultBuffer::UpdateSubresource(ID3D12Device* pDevice, ID3D12GraphicsCommandList* pCmdList)
+{
+    assert(m_subresource);
+    if (!m_subresource) return false;
+
+    size_t sizeInBytes = static_cast<size_t>(m_subresource->SlicePitch);
+
+    m_buffer = D12Helper::CreateBuffer(pDevice, sizeInBytes, D3D12_HEAP_TYPE_DEFAULT);
+    m_intermedinateBuffer = D12Helper::CreateBuffer(pDevice, sizeInBytes);
+
+    UpdateSubresources(pCmdList, m_buffer.Get(), m_intermedinateBuffer.Get(), 0, 0, 1, m_subresource);
+
+    if(m_isShaderResourceBuffer)
+        D12Helper::ChangeResourceState(pCmdList, m_buffer.Get(),
+            D3D12_RESOURCE_STATE_COPY_DEST,
+            D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE);
+    else
+        D12Helper::ChangeResourceState(pCmdList, m_buffer.Get(),
+            D3D12_RESOURCE_STATE_COPY_DEST,
+            D3D12_RESOURCE_STATE_GENERIC_READ);
+        
+    return true;
+}
+
+bool DefaultBuffer::ClearSubresource()
+{
+    if (m_subresource != nullptr)
+        delete m_subresource;
+
+    if (!m_intermedinateBuffer) return false;
+    m_intermedinateBuffer.Reset();
+
+    return true;
+}
--- DirectX12Study/Utility/DefaultBuffer.h
+++ DirectX12Study/Utility/DefaultBuffer.h
@@ -1,7 +1,38 @@
 #pragma once
+#include <d3dx12.h>
 
+using Microsoft::WRL::ComPtr;
+
+// GPU read only buffer
 class DefaultBuffer
 {
+public:
+	DefaultBuffer() = default;
+	~DefaultBuffer();
 
+	ID3D12Resource* Resource();
+	D3D12_GPU_VIRTUAL_ADDRESS GetGPUVirtualAddress();
+
+	void SetUpSubresource(const void* pData, LONG_PTR RowPitch, LONG_PTR SlicePitch,
+		bool IsShaderResourceBuffer = false);
+
+	// Only use for NON-TEXTURE buffer
+	void SetUpSubresource(const void* pData, size_t SizeInBytes);
+
+	// Update subresource to default buffer will be processed by GPU
+	// Need set up this method at GPU 
+	bool UpdateSubresource(ID3D12Device* pDevice, ID3D12GraphicsCommandList* pCmdList);
+
+	// When subresource is updated to default buffer
+	// It has no usage so clean it for better memeory usage
+	// CAUTION :
+	// - GPU need subresource to update data to default buffer
+	// - Don't use this method until GPU done updating
+	bool ClearSubresource();
+private:
+	ComPtr<ID3D12Resource> m_buffer = nullptr;
+	ComPtr<ID3D12Resource> m_intermedinateBuffer = nullptr;
+	D3D12_SUBRESOURCE_DATA* m_subresource = nullptr;
+	bool m_isShaderResourceBuffer = false;
 };
 
