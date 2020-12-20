325fe33d8e723ad05bb325fd92b9388004dd166e
Trinh Le Hai Nam <trinhlehainamjapan@gmail.com>
2020-12-20 21:48:18 +0900

    Change static class method to namespace function

--- DirectX12Study/Utility/D12Helper.h
+++ DirectX12Study/Utility/D12Helper.h
@@ -4,54 +4,52 @@
 #include <stdexcept>
 #include <unordered_map>
 
-class D12Helper
+namespace D12Helper
 {
-public:
 
-	static Microsoft::WRL::ComPtr<ID3D12Resource> CreateBuffer(ID3D12Device* pDevice, size_t sizeInBytes, D3D12_HEAP_TYPE = D3D12_HEAP_TYPE_UPLOAD);
+	Microsoft::WRL::ComPtr<ID3D12Resource> CreateBuffer(ID3D12Device* pDevice, size_t sizeInBytes, D3D12_HEAP_TYPE = D3D12_HEAP_TYPE_UPLOAD);
 
 	// (width, height, DXGI_FORMAT, D3D12_RESOURCE_FLAGS) -> D3D12_RESOURCE_DESC
-	static Microsoft::WRL::ComPtr<ID3D12Resource> CreateTexture2D(ID3D12Device* pdevice, UINT64 width, UINT height,  DXGI_FORMAT = DXGI_FORMAT_R8G8B8A8_UNORM,
+	Microsoft::WRL::ComPtr<ID3D12Resource> CreateTexture2D(ID3D12Device* pdevice, UINT64 width, UINT height,  DXGI_FORMAT = DXGI_FORMAT_R8G8B8A8_UNORM,
 		D3D12_RESOURCE_FLAGS = D3D12_RESOURCE_FLAG_NONE, D3D12_HEAP_TYPE = D3D12_HEAP_TYPE_DEFAULT, D3D12_RESOURCE_STATES = D3D12_RESOURCE_STATE_GENERIC_READ, const D3D12_CLEAR_VALUE* clearValue = nullptr);
 
-	static bool CreateDescriptorHeap(ID3D12Device* pDevice, Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>& pDescriptorHeap,
+	bool CreateDescriptorHeap(ID3D12Device* pDevice, Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>& pDescriptorHeap,
 		UINT numDesciprtor, D3D12_DESCRIPTOR_HEAP_TYPE heapType, bool isShaderVisible = false, UINT nodeMask = 0);
 
-	static void OutputFromErrorBlob(Microsoft::WRL::ComPtr<ID3DBlob>& errBlob);
+	void OutputFromErrorBlob(Microsoft::WRL::ComPtr<ID3DBlob>& errBlob);
 
-	static size_t AlignedValue(size_t value, size_t align);
+	 size_t AlignedValue(size_t value, size_t align);
 	
 	// Minimum memory block of constant buffer that hardware allows is 256 bits
 	// Multiple constant buffers must be multiple memory blocks of 256 bits
-	static size_t AlignedConstantBufferMemory(size_t byzeSize);
+	size_t AlignedConstantBufferMemory(size_t byzeSize);
 
-	static void ThrowIfFailed(HRESULT result);
+	void ThrowIfFailed(HRESULT result);
 
 	// Compile shader file at RUNTIME and convert it to bytecode for PSO (pipeline state object) use it
 	// ->Return shader's bytecode
-	static Microsoft::WRL::ComPtr<ID3DBlob> CompileShaderFromFile(const wchar_t* filePath, const char* entryName, const char* targetVersion, D3D_SHADER_MACRO* defines = nullptr);
+	Microsoft::WRL::ComPtr<ID3DBlob> CompileShaderFromFile(const wchar_t* filePath, const char* entryName, const char* targetVersion, D3D_SHADER_MACRO* defines = nullptr);
 
 	// Return nullptr if FAILED to load texture from file
-	static Microsoft::WRL::ComPtr<ID3D12Resource> CreateTextureFromFilePath(Microsoft::WRL::ComPtr<ID3D12Device>& device, const std::wstring& path);
+	Microsoft::WRL::ComPtr<ID3D12Resource> CreateTextureFromFilePath(Microsoft::WRL::ComPtr<ID3D12Device>& device, const std::wstring& path);
 
 	// Create resource has default heap type (GPU read only)
 	// Use upload buffer to UPDATE resource to default buffer
 	// *** UPLOAD BUFFER SHOULD BE EMPTY
 	// *** NEED TO KEEP UPLOAD BUFFER ALIVE UNTIL GPU UPDATE DATA 
 	//  FROM UPLOAD BUFFER TO DEFAULT BUFFER
-	static Microsoft::WRL::ComPtr<ID3D12Resource> CreateDefaultBuffer(ID3D12Device* pDevice, ID3D12GraphicsCommandList* pCmdList,
+	Microsoft::WRL::ComPtr<ID3D12Resource> CreateDefaultBuffer(ID3D12Device* pDevice, ID3D12GraphicsCommandList* pCmdList,
 		Microsoft::WRL::ComPtr<ID3D12Resource>& emptyUploadBuffer, const void* pData, size_t dataSize);
 
 	// *** NEED TO KEEP UPLOAD BUFFER ALIVE UNTIL GPU UPDATE DATA 
 	//  FROM UPLOAD BUFFER TO DEFAULT BUFFER
-	static bool UpdateDataToTextureBuffer(ID3D12Device* pDevice, ID3D12GraphicsCommandList* pCmdList,
+	bool UpdateDataToTextureBuffer(ID3D12Device* pDevice, ID3D12GraphicsCommandList* pCmdList,
 		Microsoft::WRL::ComPtr<ID3D12Resource>& textureBuffer, Microsoft::WRL::ComPtr<ID3D12Resource>& emptyUploadBuffer, const D3D12_SUBRESOURCE_DATA& subResource);
 
 	// Change resource state at GPU executing time
-	static void ChangeResourceState(ID3D12GraphicsCommandList* pCmdList, ID3D12Resource* pResource, 
+	void ChangeResourceState(ID3D12GraphicsCommandList* pCmdList, ID3D12Resource* pResource, 
 		D3D12_RESOURCE_STATES stateBefore, D3D12_RESOURCE_STATES stateAfter);
 
-private:
 	class HrException : public std::runtime_error
 	{
 	public:
@@ -59,9 +57,10 @@ private:
 		HRESULT Error() const;
 	private:
 		HRESULT m_hr;
+		friend std::string HrToString(HRESULT hr);
 	};
 
-	static std::string HrToString(HRESULT hr);
+	std::string HrToString(HRESULT hr);
 };
 
 // Buffers use for update to texture buffer
--- DirectX12Study/Utility/StringHelper.h
+++ DirectX12Study/Utility/StringHelper.h
@@ -2,15 +2,14 @@
 #include <string>
 #include <vector>
 
-class StringHelper
+namespace StringHelper
 {
-public:
-	static std::string GetTexturePathFromModelPath(const char* modelPath, const char* texturePath);
+	std::string GetTexturePathFromModelPath(const char* modelPath, const char* texturePath);
 
-	static std::wstring ConvertStringToWideString(const std::string& str); 
+	std::wstring ConvertStringToWideString(const std::string& str); 
 
-	static std::string GetFileExtension(const std::string& path);
+	std::string GetFileExtension(const std::string& path);
 
-	static std::vector<std::string> SplitFilePath(const std::string& path, const char splitter = ' *');
+	std::vector<std::string> SplitFilePath(const std::string& path, const char splitter = ' *');
 };
 
