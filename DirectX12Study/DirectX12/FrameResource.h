b818571d2876c228ce5a3b672f2ed618cc22ac1e
Trinh Le Hai Nam <trinhlehainamjapan@gmail.com>
2020-12-30 19:54:36 +0900

    Add command to Frame Resource class

--- DirectX12Study/DirectX12/FrameResource.h
+++ DirectX12Study/DirectX12/FrameResource.h
@@ -3,8 +3,11 @@
 
 struct FrameResource
 {
-	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> CmdAlloc = nullptr;
-	uint64_t FenceValue = 0;
 	FrameResource(ID3D12Device* pDevice);
+	// Each frame resource has their command allocator to store its own command list
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> CmdAlloc = nullptr;
+	// Use to check if GPU is currently working on this frame resource
+	uint64_t FenceValue = 0;
+	
 };
 
